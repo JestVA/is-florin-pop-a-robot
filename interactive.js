7f5f002ed0f775ba1f66f2ac16f3983a4ebfb17b
Dorin <d.dumitrascuta@gmail.com>
2020-05-17 01:02:00 +0100

    Add skeleton markup and take out API key from commit
    successfully queried YT  Search API

---
+++ interactive.js
@@ -0,0 +1,21 @@
+// Make a fun array of things to populate the button 
+// As if searching through all this stuff actually
+// takes a while.. which it might, considering I want
+// to do a while loop haha
+
+const whileFetching = [
+	"Found YouTube channel",
+	"He looks too young to do all this stuff",
+	"Ok, looks like we have plenty to count",
+	"Geesh, I wish I were a robot",
+	"These videos are funny!",
+	"Almost there",
+	"And the answer is",
+	"Kidding...",
+	"Tweets left to count.. too many",
+	"Sending birds",
+	"Intercepted tweet",
+	"hmm... 30 sec x many tweets",
+	"Hi Black Twitter!",
+	"Drum rolls"
+];
