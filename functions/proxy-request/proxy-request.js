c4e8824d679b888d5be50784d748f6f726cf894b
Dorin <d.dumitrascuta@gmail.com>
2020-05-22 23:19:49 +0100

    will link this to Netlify - created CLI this is really cool

--- functions/proxy-request/proxy-request.js
+++ functions/proxy-request/proxy-request.js
@@ -1,14 +1,20 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
+	console.log(process.env)
+	const protectedKey = process.env.API_KEY_YT;
+	console.log(protectedKey);
+
+	const subject = event.queryStringParameters.name || 'World'
+	
+	return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` })
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
-    }
+	}
+	
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
