d1abac7215afebad1b894b720e832c2c9d2f7c79
Dorin <d.dumitrascuta@gmail.com>
2020-05-23 00:55:32 +0100

    play around in lambda -- refactor time soon!
    Love Netlify - this is so nice

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 is-florin-pop-a-robot
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/a315f327-d92b-451c-a279-dd22afa456b2/deploy-status)](https://app.netlify.com/sites/is-florin-pop-a-robot/deploys)
+
+https://is-florin-pop-a-robot.netlify.app
--- functions/proxy-request/proxy-request.js
+++ functions/proxy-request/proxy-request.js
@@ -1,21 +1,34 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-	console.log(process.env)
-	const protectedKey = process.env.API_KEY_YT;
-	console.log(protectedKey);
+	
 
-	const subject = event.queryStringParameters.name || 'World'
+	const YT_API_KEY = process.env.YT_API_KEY;
+	const GET_CHANNEL_INFO_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCeU-1X402kT-JlLdAitxSMA&maxResults=50"
+	
+	// determine if it's a proxy for YouTube or Twitter (ideally run both for now)
+
+
+
+	// want to see how they are passed if multiple q strings
+	console.log(event.queryStringParameters)
+
+	// ok so they come as object with values as strings --- maybe use Boolean to coerce
+
+	// const subject = event.queryStringParameters.name || 'World'
 	
 	return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
+      body: JSON.stringify({ message: `Hello Devfrend!` })
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
 	}
 	
   } catch (err) {
+
+	// good to let front end know to display out of quota message
+	console.log(err.toString())
     return { statusCode: 500, body: err.toString() }
   }
 }
