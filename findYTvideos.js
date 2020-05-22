880381ababb0a322389dc30f1bd344029a2cdb75
Dorin <d.dumitrascuta@gmail.com>
2020-05-22 16:28:43 +0100

    solve duration for all YT vids of user

--- findYTvideos.js
+++ findYTvideos.js
@@ -1,10 +1,23 @@
-const getVideoIdsWithPagination = (config) => 
+const getVideoIdsWithPagination = (config = false, nextPageToken = false, contentDetails = false) => 
 {
+	let fetchNextPageQuery = '';
+	
+	if(nextPageToken)
+		fetchNextPageQuery = `&pageToken=${nextPageToken}`;
+
 	return new Promise((resolve, reject) => 
 	{
 		// open a HTTP protocol
 		const request = new XMLHttpRequest();
-		request.open('GET', `${config.URL}${config.API_KEY}`, true);
+		if(config)
+			// Will fetch Channel with video Ids
+			request.open('GET', `${config.URL}${fetchNextPageQuery}&key=${config.API_KEY}`, true);
+		else if(contentDetails)
+			// Will loop through all videos 50 at a time and fetch things like duration
+			request.open('GET', contentDetails, true);
+		else
+			return reject(false);
+
 		request.setRequestHeader('Authorization', `API Key ${config.API_KEY}`);
 		request.setRequestHeader('Accept', 'application/json');
 		request.addEventListener('load', resolve);
@@ -13,13 +26,9 @@ const getVideoIdsWithPagination = (config) =>
 			// uno dos tres quatros 
 			if(request.readyState === 4)
 			{
-				console.log(2);
 				return resolve(JSON.parse(request.response));
 			}
-
 		}
-
 		request.send();
-
 	});
 }
