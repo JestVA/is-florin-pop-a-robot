faf1b7f6af7b6b3608b0cf729e65e591eb34ab61
Dorin <d.dumitrascuta@gmail.com>
2020-05-25 12:41:41 +0100

    Give everything more context so it makes more sense

--- interactive.js
+++ interactive.js
@@ -4,17 +4,8 @@
 // to do a while loop haha
 
 const whileFetching = [
-	"Found YouTube channel...",
-	"He looks too young to do all this stuff...",
-	"Ok, looks like we have plenty to count...",
-	"Geesh, I wish I were a robot...",
-	"These videos are funny...",
-	"Almost there...",
-	"And the answer is...",
-	"Kidding...",
-	"Tweets left to count.. too many...",
-	"Sending birds...",
-	"Intercepted tweet...",
-	"hmm... 30 sec x many tweets...",
-	"Hi, Black Twitter...",
+	"Crunching YouTube stats...",
+	"Fetching Twitter user...",
+	"Adding everything up...",
+	"Preparing results..."
 ];
